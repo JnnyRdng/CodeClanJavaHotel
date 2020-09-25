eeff6f1f8c5950a1e2c8b786fc9a8715f061acbe
Jonny Reading <jonnyreading@gmail.com>
2020-09-25 13:48:51 +0100

    add and tested basic guest class

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,3 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+
+    private Guest guest;
+
+    @Before public void before() {
+        guest = new Guest("Kevin");
+    }
+
+    @Test public void getName() {
+        assertEquals("Kevin", guest.getName());
+    }
+
+    @Test public void setName() {
+        guest.setName("Perry");
+        assertEquals("Perry", guest.getName());
+    }
 
 }
