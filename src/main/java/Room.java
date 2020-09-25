76357a384a54ddc2f6c3806c40c767a3eb9e995c
Jonny Reading <jonnyreading@gmail.com>
2020-09-25 13:58:51 +0100

    add abstract Room class

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public abstract class Room {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(int capacity) {
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return this.guests;
+    }
+
+    public void setGuests(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
+
+    public void addGuest(Guest guest) {
+        this.guests.add(guest);
+    }
+
+    public int guestCount() {
+        return this.guests.size();
+    }
+
+    public boolean hasCapacity() {
+        return this.guestCount() < this.capacity;
+    }
+
 }
