4862aded85fbf2bbf3760cd2b5f22ff8fd097e55
Jonny Reading <jonnyreading@gmail.com>
2020-09-25 14:49:56 +0100

    hotel class getters and setters tested

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(String name) {
+        this.name = name;
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return this.bedrooms;
+    }
+
+    public int bedroomCount() {
+        return this.bedrooms.size();
+    }
+
+    public void setBedrooms(ArrayList<Bedroom> bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return this.conferenceRooms;
+    }
+
+    public int conferenceRoomCount() {
+        return this.conferenceRooms.size();
+    }
+
+    public void setConferenceRooms(ArrayList<ConferenceRoom> conferenceRooms) {
+        this.conferenceRooms = conferenceRooms;
+    }
+
+    public void buildBedroom(Bedroom bedroom) {
+        this.bedrooms.add(bedroom);
+    }
+
+    public void buildConferenceRoom(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,66 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    private Hotel hotel;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+    private Bedroom bedroom;
+    private ConferenceRoom conferenceRoom;
+    private Guest guest;
+
+    @Before public void before() {
+        hotel = new Hotel("Hotel Pedro III El Grande");
+        bedrooms = new ArrayList<>();
+        conferenceRooms = new ArrayList<>();
+        bedroom = new Bedroom(10, RoomType.DOUBLE);
+        conferenceRoom = new ConferenceRoom("Grand Hall", 100);
+        guest = new Guest("Alan");
+    }
+
+    @Test public void getName() {
+        assertEquals("Hotel Pedro III El Grande", hotel.getName());
+    }
+
+    @Test public void setName() {
+        hotel.setName("Travelodge");
+        assertEquals("Travelodge", hotel.getName());
+    }
+
+    @Test public void getBedrooms() {
+        assertEquals(bedrooms, hotel.getBedrooms());
+        assertEquals(0, hotel.bedroomCount());
+    }
+
+    @Test public void setBedrooms() {
+        bedrooms.add(bedroom);
+        hotel.setBedrooms(bedrooms);
+        assertEquals(1, hotel.bedroomCount());
+    }
+
+    @Test public void getConferenceRooms() {
+        assertEquals(conferenceRooms, hotel.getConferenceRooms());
+        assertEquals(0, hotel.conferenceRoomCount());
+    }
+
+    @Test public void setConferenceRooms() {
+        conferenceRooms.add(conferenceRoom);
+        hotel.setConferenceRooms(conferenceRooms);
+        assertEquals(1, hotel.conferenceRoomCount());
+    }
+
+    @Test public void buildBedroom() {
+        hotel.buildBedroom(bedroom);
+        assertEquals(1, hotel.bedroomCount());
+    }
+
+    @Test public void buildConferenceRoom() {
+        hotel.buildConferenceRoom(conferenceRoom);
+        assertEquals(1, hotel.conferenceRoomCount());
+    }
 }
