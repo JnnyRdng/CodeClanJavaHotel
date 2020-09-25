56fcc4d0c4a965dc47f944d7b9baed5d3a3a2896
Jonny Reading <jonnyreading@gmail.com>
2020-09-25 15:45:45 +0100

    add cost per night to RoomType and tested

--- src/main/java/RoomType.java
+++ src/main/java/RoomType.java
@@ -1,18 +1,24 @@
 public enum RoomType {
 
-    SINGLE(1),
-    DOUBLE(2),
-    TWIN(2),
-    FAMILY(4),
-    PENTHOUSE(2);
+    SINGLE(1, 25),
+    DOUBLE(2, 40),
+    TWIN(2, 40),
+    FAMILY(4, 75),
+    PENTHOUSE(2, 200);
 
     private final int capacity;
+    private final int costPerNight;
 
-    RoomType(int capacity) {
+    RoomType(int capacity, int costPerNight) {
         this.capacity = capacity;
+        this.costPerNight = costPerNight;
     }
 
     public int getCapacity() {
         return capacity;
+    }
+
+    public int getCost() {
+        return costPerNight;
     }
 }
--- src/test/java/RoomTypeTest.java
+++ src/test/java/RoomTypeTest.java
@@ -7,24 +7,38 @@ public class RoomTypeTest {
 
     private RoomType roomType;
 
-    @Test public void singleRoomCapacity() {
+    @Test
+    public void singleRoom() {
         roomType = RoomType.SINGLE;
+        assertEquals(25, roomType.getCost());
         assertEquals(1, roomType.getCapacity());
     }
-    @Test public void doubleRoomCapacity() {
+
+    @Test
+    public void doubleRoom() {
         roomType = RoomType.DOUBLE;
+        assertEquals(40, roomType.getCost());
         assertEquals(2, roomType.getCapacity());
     }
-    @Test public void twinRoomCapacity() {
+
+    @Test
+    public void twinRoom() {
         roomType = RoomType.TWIN;
+        assertEquals(40, roomType.getCost());
         assertEquals(2, roomType.getCapacity());
     }
-    @Test public void familyRoomCapacity() {
+
+    @Test
+    public void familyRoom() {
         roomType = RoomType.FAMILY;
+        assertEquals(75, roomType.getCost());
         assertEquals(4, roomType.getCapacity());
     }
-    @Test public void penthouseRoomCapacity() {
+
+    @Test
+    public void penthouseRoom() {
         roomType = RoomType.PENTHOUSE;
+        assertEquals(200, roomType.getCost());
         assertEquals(2, roomType.getCapacity());
     }
 }
